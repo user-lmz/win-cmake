e6507ac683a83a56d7d1057bbf7a0a659a4622ad
lmz <mingzeluo888@gmail.com>
2021-03-03 14:39:22 +0800

    first

---
+++ build/CMakeFiles/exec.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lmz/Desktop/C++/memory-model/model2/external.cpp" "/home/lmz/Desktop/C++/build/CMakeFiles/exec.dir/memory-model/model2/external.cpp.o"
+  "/home/lmz/Desktop/C++/memory-model/model2/support.cpp" "/home/lmz/Desktop/C++/build/CMakeFiles/exec.dir/memory-model/model2/support.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exec.dir/memory-model/model2/external.cpp.o"
+  "CMakeFiles/exec.dir/memory-model/model2/support.cpp.o"
+  "exec"
+  "exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
